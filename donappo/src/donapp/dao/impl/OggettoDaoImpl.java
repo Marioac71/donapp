73ddf19eab58ece2b9888e8605b204229b903ed7
Mario Totaro <mariototaro01@gmail.com>
2020-03-10 17:42:59 +0100

    ddssdds

--- donappo/src/donapp/dao/impl/OggettoDaoImpl.java
+++ donappo/src/donapp/dao/impl/OggettoDaoImpl.java
@@ -20,7 +20,7 @@ public class OggettoDaoImpl implements OggettoDao {
 	private PreparedStatement prenotaPsOgg;
 	private PreparedStatement cancellaPsPren;
 	
-	//COSTRUTTOREa
+	//COSTRUTTOREaxxh
 	public OggettoDaoImpl() {
 		String insertQry = "INSERT INTO oggetto (foto,nome,colore,descrizione,luogoritiro"
 				+ ",disponibilita,idproprietario,idprenotante,idcategoria) "
