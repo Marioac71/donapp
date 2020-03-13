04c0fcd742a56821fdddf77e8c28ba652931ff80
Alessio Angora <axellessio@gmail.com>
2020-03-13 12:15:36 +0100

    aggiunta servlet getallmyoggetti e servlet prenotaoggetto

---
+++ donappo/src/donapp/servlet/PrenotaOggetto.java
@@ -0,0 +1,66 @@
+package donapp.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import donapp.dao.impl.OggettoDaoImpl;
+import donapp.model.Oggetto;
+import donapp.model.Utente;
+
+/**
+ * Servlet implementation class PrenotaOggetto
+ */
+@WebServlet("/PrenotaOggetto")
+public class PrenotaOggetto extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PrenotaOggetto() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		OggettoDaoImpl o= new OggettoDaoImpl();
+		HttpSession session = request.getSession();
+		String username=(String)session.getAttribute("username");
+		String idOggetto=(String)session.getAttribute("idOggetto");
+		int a = Integer.parseInt(idOggetto);
+		
+		boolean flag = o.prenotaOggetto(username, a);
+		if (flag == true)
+		{
+			RequestDispatcher dispatcher = request.getRequestDispatcher("");
+			dispatcher.forward(request, response);
+		}
+		else
+		{
+			RequestDispatcher dispatcher = request.getRequestDispatcher("aggiungiT.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
